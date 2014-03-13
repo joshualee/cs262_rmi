ce8f82bc314e277f9f5cd88649b64b90343c2b2d
Perry Green <perryagreen@gmail.com>
2014-03-13 16:59:42 -0400

    pings fucking server

--- src/edu/harvard/cs262/ComputeServer/SimpleServer/Server.java
+++ src/edu/harvard/cs262/ComputeServer/SimpleServer/Server.java
@@ -1,35 +1,40 @@
 package edu.harvard.cs262.ComputeServer.SimpleServer;
 
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.rmi.registry.Registry;
-import java.rmi.registry.LocateRegistry;
-
 import edu.harvard.cs262.ComputeServer.ComputeServer;
 import edu.harvard.cs262.ComputeServer.WorkTask;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class Server implements ComputeServer {
 	private static final long serialVersionUID = 1L;
 	public Server(){
 		super();
 	}
-	
+
 	@Override
 	public Object sendWork(WorkTask work) throws RemoteException {
 		return work.doWork();
 	}
-	
-	public static void main(String args[]){
+
+  @Override
+  public boolean PingServer() throws RemoteException {
+    return true;
+  }
+
+  public static void main(String args[]){
 		try{
 			if (System.getSecurityManager()==null){
 				System.setSecurityManager(new SecurityManager());
 			}
 			Server mySrv = new Server();
 			ComputeServer stub = (ComputeServer)UnicastRemoteObject.exportObject(mySrv);
-			
+
 			Registry registry = LocateRegistry.getRegistry();
 			registry.bind("SimpleServer", stub);
-			
+
 			System.out.println("Server ready");
 		} catch (Exception e) {
 			System.err.println("Server exception: " + e.toString());
