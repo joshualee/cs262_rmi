eeca42345706f0303834437b684db8dcf7636d26
Perry Green <perryagreen@gmail.com>
2014-03-13 17:25:47 -0400

    Made client dumb

--- src/client/SimpleClient.java
+++ src/client/SimpleClient.java
@@ -1,30 +1,31 @@
 package client;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-
+import securityManagers.DumbSecurityManager;
 import workTasks.SleepTask;
 import workerServer.QueuedServer;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class SimpleClient {
-	
+
 	public static void main(String args[]) {
 		int rmiPort;
 		String rmiHost, rmiName;
 		SleepTask sleepTask1 = new SleepTask(1);
 		Integer returnVal;
-		
+
 		if (args.length != 3) {
 			System.err.println("usage: java SimpleClient host port rminame");
 			System.exit(1);
 		}
-		
+
 		rmiHost = args[0];
 		rmiPort = Integer.parseInt(args[1]);
 		rmiName = args[2];
-		
+
         if (System.getSecurityManager() == null) {
-            System.setSecurityManager(new SecurityManager());
+            System.setSecurityManager(new DumbSecurityManager());
         }
         try {
             Registry registry = LocateRegistry.getRegistry(rmiHost, rmiPort);
@@ -32,12 +33,12 @@ public class SimpleClient {
 
             // make some tasks
             returnVal = (Integer) server.sendWork(sleepTask1);
-            
+
             System.out.println(returnVal);
-            
+
         } catch (Exception e) {
             System.err.println("Simple Client exception:");
             e.printStackTrace();
         }
-    } 
+    }
 }
