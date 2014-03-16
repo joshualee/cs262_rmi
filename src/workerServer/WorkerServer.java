40bee5f27440d064c9fef5158e5834de3059ef2d
Andrew Liu <abliu06@gmail.com>
2014-03-16 16:51:48 -0400

    Added worker server code to register self with queued server

--- src/workerServer/WorkerServer.java
+++ src/workerServer/WorkerServer.java
@@ -2,13 +2,17 @@ package workerServer;
 
 import edu.harvard.cs262.ComputeServer.ComputeServer;
 import edu.harvard.cs262.ComputeServer.WorkTask;
+import securityManagers.DumbSecurityManager;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.RemoteException;
 
 /**
  * Created by perry on 3/13/14.
  */
 public class WorkerServer implements ComputeServer {
+
   @Override
   public Object sendWork(WorkTask work) throws RemoteException {
     return work.doWork();
@@ -19,11 +23,30 @@ public class WorkerServer implements ComputeServer {
     return true;
   }
 
-  private boolean registerSelf() {
-    return true;
-  }
+  public static void main(String args[]) {
+    int rmiPort;
+    String rmiHost, rmiName;
 
-  public static void main() {
+    if (args.length != 3) {
+      System.err.println("usage: java WorkerServer host port rminame");
+      System.exit(1);
+    }
 
+    rmiHost = args[0];
+    rmiPort = Integer.parseInt(args[1]);
+    rmiName = args[2];
+
+    if (System.getSecurityManager() == null) {
+      System.setSecurityManager(new DumbSecurityManager());
+    }
+    try {
+      Registry registry = LocateRegistry.getRegistry(rmiHost, rmiPort);
+      QueuedServer queueServer = (QueuedServer) registry.lookup(rmiName);
+      WorkerServer workerServer = new WorkerServer();
+      queueServer.registerWorker(workerServer);
+    } catch (Exception e) {
+      System.err.println("Worker Server exception:");
+      e.printStackTrace();
+    }
   }
 }
