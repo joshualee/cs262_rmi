89de1970dd12bbaa03c5aaf7f5f5bd1754100e3b
Perry Green <perryagreen@gmail.com>
2014-03-25 01:34:47 -0400

    Comments to client things

--- src/edu/harvard/cs262/ComputeServer/WorkAttempter.java
+++ src/edu/harvard/cs262/ComputeServer/WorkAttempter.java
@@ -7,7 +7,16 @@ package edu.harvard.cs262.ComputeServer;
  */
 
 public interface WorkAttempter {
-
+  /**
+   * Attempts to have worker perform WorkTask
+   * @param worker The worker to perform the WorkTask
+   * @param work The WorkTask
+   * @param maxAttempts The maximum number of times the worker can fail to respond to ping in time
+   * @param pingTimeout The amount of time given to respond to each ping, before considered a failed attempt
+   * @return The result of the WorkTask
+   * @throws WorkFailedException If the worked fails to respond to ping within pingTimeout seconds more than
+   * maxAttempts times.
+   */
   public Object attemptWork(ComputeServer worker, WorkTask work, int maxAttempts, int pingTimeout) throws WorkFailedException;
 
 }
